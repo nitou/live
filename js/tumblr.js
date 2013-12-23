3baf2e8bdfd87d6c9563686ad7f1387244d361fd
nitou <nitouy@gmail.com>
2013-12-23 22:13:34 +0900

    test

--- js/tumblr.js
+++ js/tumblr.js
@@ -4,14 +4,13 @@ function getPost($http){
 	this.loadTumblr = function(callback){
 		var _api  = "http://api.tumblr.com/v2/",
 			_user = "blog/yj-creative.tumblr.com/posts",
-			_key  = "?api_key=",
+			_key  = "?api_key=lHZfs3MnhHSI1QkbaZJWrdrmnjgPwn8zEniLUdBHkfsmTQd7R9",
 			_callback = "&callback=JSON_CALLBACK",
 			_url  = _api + _user + _key + _callback;
 
 		$http.jsonp(_url)
-		.success(function(response){
-            console.log(response);
-			callback(response.response);
+		.success(function(data){
+			callback(data.response);
 		});
 	};
 }
@@ -21,23 +20,18 @@ function AppController($scope, $http, $sce){
 	$scope.onLoad = function(){
 		$scope.posts = [];
 		var api = new getPost($http);
-		api.loadTumblr(function(response){
+		api.loadTumblr(function(data){
 			// blog info
-			$scope.title = response.blog.title;
-			$scope.description = response.blog.description;
-			$scope.name  = response.blog.name;
+			$scope.title = data.blog.title;
+			$scope.description = data.blog.description;
+			$scope.name  = data.blog.name;
 
 			// blog post
-			$scope.posts = response.posts;
+			$scope.posts = data.posts;
 			// テキスト部分がタグ入りで入ってくるので処理
 			angular.forEach($scope.posts, function(value, key){
 				$scope.posts[key].body = $sce.trustAsHtml( value.body );
 			});
 		});
-	};
-
-	$scope.openBody = function(e){
-		e.preventDefault();
-		return 'on';
 	};
 }
