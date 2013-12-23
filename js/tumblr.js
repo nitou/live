39446089b6378ce82361738571dce1fb01c99238
nitou <nitouy@gmail.com>
2013-12-23 16:11:11 +0900

    js

--- js/tumblr.js
+++ js/tumblr.js
@@ -4,12 +4,13 @@ function getPost($http){
 	this.loadTumblr = function(callback){
 		var _api  = "http://api.tumblr.com/v2/",
 			_user = "blog/yj-creative.tumblr.com/posts",
-			_key  = "?api_key=X7ezD7uWgkAh3rNmbM34E9qPo98XKOF3pN8vb9teRMebhmog2L",
+			_key  = "?api_key=",
 			_callback = "&callback=JSON_CALLBACK",
 			_url  = _api + _user + _key + _callback;
 
 		$http.jsonp(_url)
 		.success(function(response){
+            console.log(response);
 			callback(response.response);
 		});
 	};
