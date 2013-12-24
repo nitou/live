cf2beb1d7b6acc400635c7d832948ea335391bcc
neutry <neutry@gmail.com>
2013-12-25 04:18:57 +0900

    end

--- config.rb
+++ config.rb
@@ -8,8 +8,8 @@ images_dir = "img"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-#output_style = :compressed
-output_style = :expanded
+output_style = :compressed
+#output_style = :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
